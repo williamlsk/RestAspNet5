8d7abd98a1e9a5c2e02443a73a8e0bc32f30275e
William Leandro da Silva <william_lds@hotmail.com>
2021-03-07 22:36:37 -0300

    Atualizando banco usando Migrations

--- 04_RestWithASPNETUdemy_ConnectingToDatabase/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 04_RestWithASPNETUdemy_ConnectingToDatabase/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -9,26 +9,38 @@ using RestWithASPNETUdemy.Busines;
 using RestWithASPNETUdemy.Busines.Implementations;
 using RestWithASPNETUdemy.Repository;
 using RestWithASPNETUdemy.Repository.Implementations;
+using Serilog;
+using System;
+using System.Collections.Generic;
 
 namespace RestWithASPNETUdemy
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           
 
             services.AddControllers();
 
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
 
             //Versioning API
             services.AddApiVersioning();
@@ -36,6 +48,26 @@ namespace RestWithASPNETUdemy
             //Dependency Injection
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+        }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database Migration failed", ex);
+                throw;
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ 04_RestWithASPNETUdemy_ConnectingToDatabase/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO `person` VALUES 
+(1,'Ayrton','Senna','São Paulo','Male'),
+(2,'Leonardo','Da Vincci','Anchiano - Italy','Male'),
+(3,'Mahatma','Gandhi','Porbandar - India','Male'),
+(4,'Mohamed Ali','Gandhi','Kentucky - USA','Male'),
+(5,'Nelson','Mendela','Mvezo - South Africa','Male');
+
