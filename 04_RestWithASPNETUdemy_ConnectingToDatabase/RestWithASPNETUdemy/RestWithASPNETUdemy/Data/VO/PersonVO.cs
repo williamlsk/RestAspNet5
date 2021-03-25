b7e3d54faad5f7c803e8453bd00ac6d82b24be35
William Leandro da Silva <william_lds@hotmail.com>
2021-03-25 14:10:55 -0300

    Custom Serialization com Json

--- 04_RestWithASPNETUdemy_ConnectingToDatabase/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ 04_RestWithASPNETUdemy_ConnectingToDatabase/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,12 +1,19 @@
-﻿namespace RestWithASPNETUdemy.Data.VO
-{
+﻿using System.Text.Json.Serialization;
 
+namespace RestWithASPNETUdemy.Data.VO
+{
+    
     public class PersonVO 
     {
+        //[JsonPropertyName("code")]
         public long Id { get; set; }
+        //[JsonPropertyName("Name")]
         public string FirstName { get; set; }
+        //[JsonPropertyName("Last_Name")]
         public string LastName { get; set; }
+        //[JsonIgnore]
         public string Address { get; set; }
+        //[JsonPropertyName("Sex")]
         public string Gender { get; set; }
     }
 }
